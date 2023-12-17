0a1ba33aed12a75295d782f3f863e295696acbcf
jonas1702 <Jonaskrebber074@gmail.com>
2023-12-17 20:17:27 +0100

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style.css';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='footer-overlay'></div>
+            <h1>This is a Footer!!</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum quisquam eveniet odit illo, ad harum laborum. Incidunt eveniet odio voluptate cumque qui vel, molestiae eius!</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,6 +6,7 @@ import SwitchBtn from "../components/SwitchBtn"
 import DeleteCard from "../components/DeleteBook"
 import BookTable from "../components/BookTable"
 import BookCards from "../components/BookCards"
+import Footer from "../components/Footer"
 
 
 const Home = () => {
@@ -27,20 +28,23 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container">
-      <div className="top-row">
-        <h1>Book Database</h1>
-        <div className="top-row-container">
-          <Link to='/books/create'>
-            <button>Create Book</button>
-          </Link>
-          <SwitchBtn isChecked={isChecked} setIsChecked={setIsChecked} />
+    <main>
+      <div className="container">
+        <div className="top-row">
+          <h1>Book Database</h1>
+          <div className="top-row-container">
+            <Link to='/books/create'>
+              <button>Create Book</button>
+            </Link>
+            <SwitchBtn isChecked={isChecked} setIsChecked={setIsChecked} />
+          </div>
         </div>
+          { !isChecked ?  <BookTable books={books} setBooks={setBooks} /> : <BookCards books={books} setBooks={setBooks} /> }
+          
+        <div className="overlay"></div>
       </div>
-        { !isChecked ?  <BookTable books={books} setBooks={setBooks} /> : <BookCards books={books} setBooks={setBooks} /> }
-        
-      <div className="overlay"></div>
-    </div>
+      <Footer />
+    </main>
   )
 }
 
